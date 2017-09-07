41a37dfeaa9b2ec21670fd2ec187ebca3c42bc80
ashish9342 <asrawat@deloitte.com>
2017-09-07 18:01:18 +0530

    Bug | crossorigin

--- app/config.js
+++ app/config.js
@@ -1,3 +1,3 @@
 var CONFIG = {
-  'contextURL'                    : 'https://philipschatbot.10.21.251.138.net/api/'
+  'contextURL'                    : 'https://philipschatbot.mybluemix.net/api/'
 };
